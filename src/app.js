b876b0a45b179d91f475c6ffbc7c4920b09cf02f
LeidyTatianaMartinezA <128093014+LeidyTatianaMartinezA@users.noreply.github.com>
2023-04-05 14:47:37 -0500

    Comandos servidor

--- src/app.js
+++ src/app.js
@@ -3,6 +3,17 @@ import {port} from './config/index.js';
 
 const app = express();
 
-app.listen(port, () => {
+app.get('/', (request, response, error)=>{
+  
+  response.send('status: ok')
+})
+
+app.listen(port, (error) => {
+
+  if(error) {
+    console.log('Server error: Failed');
+    process.exit(1);
+  }
+  console.log(`Server listening port ${port}`)
 
 })
